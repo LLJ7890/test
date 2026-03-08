b23c269b06b65ae1107906ef88aaea5a415f11a5
lilinjun <llj8882026@163.com>
2026-03-08 21:14:58 +0800

    eRyder.

---
+++ src/text/method.java
@@ -0,0 +1,32 @@
+package text;
+
+import java.net.SocketOption;
+import java.sql.SQLOutput;
+
+public class method {
+    //方法
+    int a=10;
+    int b=20;
+    // 无参数、无返回值的方法
+    sayHello;
+public void sayHello() {
+    System.out.println("Hello!");
+
+}
+
+// 有参数、有返回值的方法
+    method abc = new method();
+int result = abc.add(a, b);
+public static int add(int a, int b) {
+
+    return a + b;
+}
+
+
+// 静态方法
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    // 程序入口点
+}
+
+}
