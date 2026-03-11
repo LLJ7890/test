e8fab892a41ba77729806562bcf55fbd85ccd15d
lilinjun <llj8882026@163.com>
2026-03-11 09:57:23 +0800

    eRyder.

--- src/ERyder.java
+++ src/ERyder.java
@@ -1,59 +1,84 @@
 public class ERyder {
+   private String bikeId;
+   private int batterer;
+   private boolean isAvailable;
+   private double kmDriven;
+   private String company_Name="hello eRyder";
+   private double base_Fare=1.0;
+   private double per_Minute_Fare=0.5;
+   private final String linked_Account;
+   private final String linked_PhoneNumber;
+   private double total_Fare;
+   private int totalUsageINMinutes;
+   public ERyder(int batterylevel, String bikeId, boolean isAvailable, double kmDriven) {
+      this.batterer = batterylevel;
+      this.bikeId = bikeId;
+      this.isAvailable = isAvailable;
+      this.kmDriven = kmDriven;
+      this.linked_Account = String.valueOf((int)(Math.random()*100000));
+      this.linked_PhoneNumber = "666"+ (int) (Math.random() * 10000000);
+   }
 
-    private int bikeID;
-    private int batteryLevel;
-    private boolean isAvailable;
-    private double kmDriven;
+   public ERyder(int batterylevel, String bikeId, boolean isAvailable, double kmDriven,String linked_Account, String linked_PhoneNumber) {
+      this.batterer = batterylevel;
+      this.linked_Account = linked_Account;
+      this.linked_PhoneNumber = linked_PhoneNumber;
+      this.bikeId = bikeId;
+      this.kmDriven = kmDriven;
+      this.isAvailable = isAvailable;
+   }
 
+   public int getBatterylevel() {
+      return batterer;
+   }
 
-    public static final String COMPANY_NAME = "eRyder";
+   public void setBatterylevel(int batterylevel) {
+      if(batterylevel < 0||batterylevel > 100){
+         System.out.println("Baterylevel must be between 0 and 100");
+         return;
+      }
+      this.batterer = batterylevel;
+   }
 
-    public ERyder() {
-        this.bikeID = 0;
-        this.batteryLevel = 100;
-        this.isAvailable = true;
-        this.kmDriven = 0.0;
-    }
+   public String getBikeId() {
+      return bikeId;
+   }
 
-    // set
-    public ERyder(int id, int battery, boolean available, double km) {
-        this.bikeID = id;
-        this.batteryLevel = battery;
-        this.isAvailable = available;
-        this.kmDriven = km;
-    }
+   public void setBikeId(String bikeId) {
+      this.bikeId = bikeId;
+   }
 
-    // Check
-    public void ride() {
-        if (isAvailable && batteryLevel > 0) {
-            System.out.println("Bike #" + bikeID + " is now available for riding!");
-        } else {
-            System.out.println("Bike #" + bikeID + " is not available for riding now!");
-        }
-    }
+   public boolean isAvailable() {
+      return isAvailable;
+   }
 
+   public void setAvailable(boolean available) {
+      isAvailable = available;
+   }
 
-    public void printBikeDetails() {
-        System.out.println("- Bike Information -");
-        System.out.println("Bike ID: " + bikeID);
-        System.out.println("Battery Level: " + batteryLevel + "%");
-        System.out.println("Availability: " + (isAvailable ? "Yes" : "No"));
-        System.out.println("Kilometers Driven: " + kmDriven + " km");
-        System.out.println("Company: " + COMPANY_NAME);
-        System.out.println("----------");
+   public double getKmDriven() {
+      return kmDriven;
+   }
 
-    }
-
-    public void setBatteryLevel(int newBattery) {
-        if (newBattery >= 0 && newBattery <= 100) {
-            batteryLevel = newBattery;
-        } else {
-            System.out.println("Error: Battery level must be between 0 and 100!");
-        }
-    }
-
-
-    public int getBatteryLevel() {
-        return batteryLevel;
-    }
+   public void setKmDriven(double kmDriven) {
+      this.kmDriven = kmDriven;
+   }
+   public void ride(){
+      if(isAvailable&& batterer >0&& batterer <100){
+         System.out.println("ID:"+bikeId+" this bike is available");
+      }else{
+         System.out.println("ID:"+bikeId+" this bike is not available");
+      }
+   }
+   public void printBikeDetails(){
+      System.out.println("ID:"+bikeId+"\nbatterer:"+ batterer +"\nisavailable:"+isAvailable+"\nkmDriven:"+kmDriven+"km"+"\ncompany:"+company_Name);
+   }
+   public void printRideDetails(int usageInMinutes){
+      System.out.println("account:"+linked_Account+"\nphone number:"+linked_PhoneNumber+"\nID:"+bikeId
+            +"\ntotal Fare:"+calculateFare(usageInMinutes)+"￥\ntotalUsageINMinutes:"+usageInMinutes+" minutes"
+      );
+   }
+   private double calculateFare(int usageInMinutes){
+      return total_Fare=base_Fare+per_Minute_Fare*usageInMinutes;
+   }
 }
--- src/ERyderMain.java
+++ src/ERyderMain.java
@@ -1,25 +1,24 @@
 public class ERyderMain {
-    public static void main(String[] args) {
 
-        ERyder bike1 = new ERyder();
-        System.out.println("Default information for Bike 1:");
-        bike1.printBikeDetails();
-        bike1.ride();
+    private static final int DEFAULT_BATTERY_LEVEL = 90;
+    private static final String DEFAULT_BIKE_NAME = "bike1";
+    private static final boolean DEFAULT_IS_ELECTRIC = true;
+    private static final double DEFAULT_BATTERY_CAPACITY = 1.0;
+    private static final int RIDE_DURATION = 20;
 
-        System.out.println();
-
-        // Create second bike - custom values
-        ERyder bike2 = new ERyder(101, 75, false, 12.8);
-        System.out.println("Custom information for Bike 2:");
-        bike2.printBikeDetails();
-        bike2.ride();
-
-        System.out.println();
-
-
-        bike1.setBatteryLevel(85);
-        System.out.println("Bike 1 battery after setting: " + bike1.getBatteryLevel() + "%");
-        bike1.setBatteryLevel(120);
-        System.out.println("Bike 1 current battery: " + bike1.getBatteryLevel() + "%");
+   public static void main(String[] args) {
+        try {
+            ERyder bike1 = new ERyder(DEFAULT_BATTERY_LEVEL, DEFAULT_BIKE_NAME, DEFAULT_IS_ELECTRIC, DEFAULT_BATTERY_CAPACITY);
+            
+            bike1.printBikeDetails();
+            bike1.ride();
+            
+            System.out.println("------");
+            bike1.printRideDetails(RIDE_DURATION);
+            
+        } catch (Exception e) {
+            System.err.println("The program runs with an error：" + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
